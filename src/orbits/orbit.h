867e7e7af0d624112a4b8b15ec7296647014cb07
Tuqz <jamesrawson628@gmail.com>
2015-07-28 17:38:14 +0100

    Redesigned orbit handling

--- src/orbits/orbit.h
+++ src/orbits/orbit.h
@@ -6,14 +6,23 @@
 namespace helion {
 	class Orbit {
 	public:
-		Orbit(const Orbitable &p, double semimajor = 0, double eccentricity = 0, double inclination = 0, double longitude = 0, double argument = 0, double time_of_periapsis = 0);
+		Orbit(Orbitable &p, double semimajor = 0, double eccentricity = 0, double inclination = 0, double longitude = 0, double argument = 0, double time_of_periapsis = 0);
 		//semimajor is as normal for ellipses, the periapsis distance for parabolas, and half the spacing between branchs of a hyperbola
-		const double semimajor, eccentricity, inclination, long_asc_node, arg_of_periapsis, time_of_periapsis, period;
-		const Orbitable &parent;
 		glm::vec3 position(double theta) const;
+		double getSemimajor();
+		double getEccentricity();
+		double getInclination();
+		double getLongitude();
+		double getArgument();
+		double getTimeOfPeriapsis();
+		double getPeriod();
+		Orbitable& getParent();
 	private:
+		Orbitable &parent;
+		double semimajor, eccentricity, inclination, long_asc_node, arg_of_periapsis, time_of_periapsis, period;
 		std::vector<glm::vec3> orbital_points; //Precalculated points for rendering purposes
-		glm::vec3 get_position(double r, double theta) const;
+		glm::vec3 getPosition(double r, double theta) const;
 		double altitude(double theta) const;
+		void calculatePoints();
 	};
 }
