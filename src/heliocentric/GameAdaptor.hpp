5c8f1b8eabe72c89bc989ca8d3fd7f066781cad0
Ale Strooisma <ale@strooisma>
2015-07-31 20:03:21 +0200

    Added documentation

---
+++ src/heliocentric/GameAdaptor.hpp
@@ -0,0 +1,43 @@
+/* 
+ * File:   GameAdaptor.hpp
+ * Author: Ale Strooisma
+ *
+ * Created on July 31, 2015, 7:34 PM
+ */
+
+#ifndef GAMEADAPTOR_HPP
+#define	GAMEADAPTOR_HPP
+
+#include "GameInterface.hpp"
+
+/**
+ * An implementation of GameInterface that has an empty implementation of all 
+ * methods. `shouldStop` always returns `true` and `windowClosed` always returns
+ * `true`. Additionally a pointer to a `Game3D`-type is provided as a member 
+ * variable, including a getter and setter, the latter of which is required.
+ */
+class GameAdaptor : GameInterface {
+protected:
+	Game3D* game;
+public:
+	GameAdaptor();
+	GameAdaptor(const GameAdaptor& orig) = delete;
+	virtual ~GameAdaptor();
+	virtual void init();
+	virtual void update(double dt);
+	virtual void renderHUD(glm::mat4 base);
+	virtual void renderWorld(glm::mat4 base);
+	virtual void shutdown();
+	virtual bool shouldStop();
+	virtual void windowResized(int width, int height);
+	virtual bool windowClosed();
+	virtual Game3D* getGame() const {
+		return game;
+	}
+	virtual void setGame(Game3D* game) {
+		this->game = game;
+	}
+};
+
+#endif	/* GAMEADAPTOR_HPP */
+
--- src/heliocentric/RenderManager.hpp
+++ src/heliocentric/RenderManager.hpp
@@ -34,25 +34,102 @@ private:
 	glm::vec3 sunPosition;
 	glm::vec4 sunCameraPosition;
 public:
+	/**
+	 * Creates an instance of `RenderManager`.
+	 * 
+     * @param game the Game3D instance for this game
+     * @param createDefaultRenderer if true, a `DefaultRenderer` is created with
+	 * the default shaders provided by Heliocentric
+     */
 	RenderManager(Game3D& game, bool createDefaultRenderer = true);
 	RenderManager(const RenderManager&) = delete;
 	virtual ~RenderManager();
+	/**
+	 * Create the default renderer, as done by default by the constructor. 
+     * @return a pointer to the new renderer
+     */
 	MeshRenderer* createDefaultRenderer();
+	/**
+	 * Creates a renderer of the type `DefaultRenderer` with a shader program 
+	 * created from the given shader sources.
+	 * 
+     * @param vertexShader the source file of the vertex shader
+     * @param geometryShader the source file of the geometry shader
+     * @param fragmentShader the source file of the fragment shader
+     * @param attributes a list of vertex attributes
+     * @return a pointer to the new renderer
+     */
 	MeshRenderer* createRenderer(std::string vertexShader, std::string geometryShader,
 			std::string fragmentShader, std::vector<std::string>* attributes = nullptr);
+	/**
+	 * Creates a renderer of the type `DefaultRenderer` with a shader program 
+	 * created from the given shader sources.
+	 * 
+     * @param vertexShader the source file of the vertex shader
+     * @param fragmentShader the source file of the fragment shader
+     * @param attributes a list of vertex attributes
+     * @return a pointer to the new renderer
+     */
 	MeshRenderer* createRenderer(std::string vertexShader, std::string fragmentShader,
 			std::vector<std::string>* attributes = nullptr);
-	void initializeProgram(ShaderProgram& program);
+	/**
+	 * Render the scene graph. 
+	 * Additionally this method uploads changed lighting properties.
+	 * 
+     * @param base the base matrix
+     */
 	void render(glm::mat4 base);
+	/**
+	 * Returns the current value for the gamma correction.
+	 * 
+     * @return the gamma correction value
+     */
 	float getGamma() const {
 		return gamma;
 	}
+	/**
+	 * Set the value for gamma correction. Note that this expects the value to 
+	 * correct for, as is done in any video game, not the gamma correction value.
+	 * 
+     * @param gamma the value for gamma correction
+     */
 	void setGamma(float gamma);
+	/**
+	 * Set parameters for the lighting - WILL MOST LIKELY CHANGE!
+	 * 
+     * @param sunLightColor
+     * @param ambientIntensity
+     * @param halfIntensityDistance
+     */
 	void setLightingProperties(glm::vec3 sunLightColor, float ambientIntensity, float halfIntensityDistance);
+	/**
+	 * Set the source location for the main lighting - WILL LIKELY CHANGE!
+	 * 
+     * @param sunPosition the location of the point source
+     */
 	void setSunPosition(glm::vec3 sunPosition);
+	/**
+	 * Add the game object to the scene graph to be rendered using the given 
+	 * mesh and the default renderer. The default renderer is the first renderer
+	 * created with this instance of `RenderManager`.
+	 * 
+     * @param obj the object to be rendered
+     * @param mesh the mesh representing the object
+     * @return the newly created scene graph node
+     */
 	Spatial* addToSceneGraph(GameObject& obj, Mesh& mesh);
+	/**
+	 * Add the game object to the scene graph to be rendered using the given 
+	 * mesh and renderer.
+	 * 
+     * @param obj the object to be rendered
+     * @param mesh the mesh representing the object
+     * @param the renderer that is to be used to render the mesh
+     * @return the newly created scene graph node
+     */
 	Spatial* addToSceneGraph(GameObject& obj, Mesh& mesh, MeshRenderer& renderer);
 private:
+	void initializeProgram(ShaderProgram& program);
 	void forall (void (RenderManager::*f)(ShaderProgram&));
 	void applyGamma(ShaderProgram& program);
 	void uploadCameraToClipMatrix(ShaderProgram& program);
